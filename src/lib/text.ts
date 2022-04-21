35f0c77918f0595e1975770f0822486831aeb3ac
Sanzhar <sanch941@gmail.com>
2022-04-21 18:27:00 +0600

    Add media prop to text component

--- src/lib/text.ts
+++ src/lib/text.ts
@@ -1,16 +1,14 @@
 import { pxRem } from './px-rem';
 import styled, { css } from 'styled-components';
 import { getDimension } from './get-dimension';
+import { media } from './media';
 
-export const Text = styled.p<Props>`
-    ${({ fz, lh, $color, fw, $width, ta }) => css`
-        font-size: ${fz ? getDimension(fz) : pxRem(16)};
-        line-height: ${Boolean(lh) && getDimension(lh)};
-        color: ${$color};
-        font-weight: ${fw};
-        width: ${Boolean($width) && getDimension($width)};
-        text-align: ${ta};
-    `}
+export const Text = styled.p<ComponentProps>`
+    ${(props) => getStyles(props)};
+
+    ${media.sm} {
+        ${({ sm }) => getStyles(sm)};
+    }
 `;
 
 interface Props {
@@ -25,3 +23,16 @@ interface Props {
     ta?: string;
 }
 export type TextProps = Props;
+
+interface ComponentProps extends Props {
+    sm?: Props;
+}
+
+const getStyles = ({ fz, lh, $color, fw, $width, ta }: Props) => css`
+    font-size: ${fz ? getDimension(fz) : pxRem(16)};
+    line-height: ${Boolean(lh) && getDimension(lh)};
+    color: ${$color};
+    font-weight: ${fw};
+    width: ${Boolean($width) && getDimension($width)};
+    text-align: ${ta};
+`;
