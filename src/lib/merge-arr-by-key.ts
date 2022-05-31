9bd0d44da7859053cc7d8a0e12d19671feaf166a
Sanzhar <sanch941@gmail.com>
2022-05-31 17:47:01 +0600

    Refactor(merge-arr-by-key): Add new prop to selector 2

--- src/lib/merge-arr-by-key.ts
+++ src/lib/merge-arr-by-key.ts
@@ -1,5 +1,10 @@
-export const mergeArrByKey = (a1: any[], a2: any[], selector: string) =>
+export const mergeArrByKey = (
+    a1: any[],
+    a2: any[],
+    selector1: string,
+    selector2?: string
+) =>
     a1.map((t1) => ({
         ...t1,
-        ...a2.find((t2) => t2[selector] === t1[selector])
+        ...a2.find((t2) => t1[selector1] === t2[selector2 || selector1])
     }));
