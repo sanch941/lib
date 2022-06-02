9fd45b779f71e402dbb5eea56ab50ed15be5b872
Sanzhar <sanch941@gmail.com>
2022-06-03 01:08:45 +0600

    Feat(text): Add new prop whs - white-space

--- src/lib/text.ts
+++ src/lib/text.ts
@@ -20,6 +20,7 @@ interface Props {
     fw?: number;
     $width?: number | string;
     ta?: string;
+    whs?: string;
 }
 export type TextProps = Props;
 
@@ -27,11 +28,12 @@ interface ComponentProps extends Props {
     sm?: Props;
 }
 
-const getStyles = ({ fz, lh, $color, fw, $width, ta }: Props = {}) => css`
+const getStyles = ({ fz, lh, $color, fw, $width, ta, whs }: Props = {}) => css`
     font-size: ${getDimension(fz)};
     line-height: ${getDimension(lh)};
     color: ${$color};
     font-weight: ${fw};
     width: ${getDimension($width)};
     text-align: ${ta};
+    white-space: ${whs};
 `;
